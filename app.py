833c931d9834659b4e660f5107fe787a4f05dcff
karthixvb <luongvvhb@gmail.com>
2025-10-24 18:56:30 +0700

    Update app.py

--- app.py
+++ app.py
@@ -264,6 +264,7 @@ def health_check():
     return jsonify({'status': 'healthy', 'timestamp': datetime.utcnow().isoformat()})
 
 if __name__ == '__main__':
-    port = int(os.getenv('FLASK_PORT', 8080))
+    port = int(os.getenv("PORT", os.getenv("FLASK_PORT", 8080)))
 
     app.run(host='0.0.0.0', port=port, debug=True)
+
